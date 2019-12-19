f4fb3c3d9e0ae963ad2af8f319952e5c59ffeea6
Ozge <bayramozinal@gmail.com>
2019-12-20 00:04:53 +0300

    BuzzHandler TCs ++

---
+++ src/test/java/com/bank/handler/BuzzHandlerTest.java
@@ -0,0 +1,47 @@
+package com.bank.handler;
+
+import com.bank.helper.impl.CommandInvoker;
+import com.bank.service.ServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BuzzHandlerTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Mock
+    private ServiceImpl mockedService;
+
+    private BuzzHandler handler;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        handler = new BuzzHandler(mockedService, mockedCommandInvoker);
+    }
+
+    @Test
+    public void shouldReturnTrue_whenRelevantParameterPassed() {
+        Mockito.when(mockedService.isBuzz(Mockito.anyInt())).thenReturn(true);
+        Mockito.doNothing().when(mockedCommandInvoker).execute(Mockito.anyString());
+
+        boolean actual = handler.execute(Mockito.anyInt());
+
+        Mockito.verify(mockedService, Mockito.times(1)).isBuzz(Mockito.anyInt());
+        Mockito.verify(mockedCommandInvoker, Mockito.times(1)).execute(Mockito.anyString());
+        Mockito.verifyNoMoreInteractions(mockedService);
+        Mockito.verifyNoMoreInteractions(mockedCommandInvoker);
+
+        Assertions.assertThat(actual).isTrue();
+    }
+}
