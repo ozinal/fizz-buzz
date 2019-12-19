f3b57deb3099e479894b006d3b8a57874328247b
Ozge <bayramozinal@gmail.com>
2019-12-20 00:16:16 +0300

    DefaultHandler comments ++

--- src/main/java/com/bank/handler/DefaultHandler.java
+++ src/main/java/com/bank/handler/DefaultHandler.java
@@ -3,7 +3,12 @@ package com.bank.handler;
 import com.bank.helper.impl.CommandInvoker;
 import com.bank.service.Service;
 
-
+/**
+ * Implements Default handler logic and receives service implementation for Default
+ * logic (non-Fizz, non-Buzz, non-FizzBuzz)
+ * It also takes invoker implementation to execute relevant task.
+ *
+ */
 public class DefaultHandler extends AbstractHandler {
 
     private Service service;
@@ -14,6 +19,12 @@ public class DefaultHandler extends AbstractHandler {
         this.commandInvoker = invoker;
     }
 
+    /**
+     * Responsible to compare service outcome and execute invokers implementation.
+     *
+     * @param i
+     * @return
+     */
     @Override
     protected boolean execute(int i) {
         if(!service.isFizz(i) && !service.isBuzz(i)) {
