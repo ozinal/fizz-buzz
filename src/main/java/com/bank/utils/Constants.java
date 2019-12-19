b24949216cc87a618f68c0b86bf15da71bb90ae0
Ozge <bayramozinal@gmail.com>
2019-12-19 23:30:04 +0300

    Additional TCs for Constants ++

--- src/main/java/com/bank/utils/Constants.java
+++ src/main/java/com/bank/utils/Constants.java
@@ -8,4 +8,10 @@ public class Constants {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
 
+    public static final String STR_THREE = "3";
+    public static final String STR_FIVE = "5";
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
 }
--- src/test/java/com/bank/utils/ConstantsTest.java
+++ src/test/java/com/bank/utils/ConstantsTest.java
@@ -21,5 +21,12 @@ public class ConstantsTest {
         Assertions.assertThat(constants.FIZZ_BUZZ).isEqualTo("FizzBuzz");
         Assertions.assertThat(constants.FIZZ).isEqualTo("Fizz");
         Assertions.assertThat(constants.BUZZ).isEqualTo("Buzz");
+
+        Assertions.assertThat(constants.STR_THREE).isEqualTo("3");
+        Assertions.assertThat(constants.STR_FIVE).isEqualTo("5");
+
+        Assertions.assertThat(constants.ANSI_RESET).isEqualTo("\u001B[0m");
+        Assertions.assertThat(constants.ANSI_RED).isEqualTo("\u001B[31m");
+        Assertions.assertThat(constants.ANSI_BLUE).isEqualTo("\u001B[34m");
     }
 }
