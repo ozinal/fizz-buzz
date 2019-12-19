34897313d47713002a8f90387617da0176ae3585
Ozge <bayramozinal@gmail.com>
2019-12-19 23:29:27 +0300

    Constants Util +

---
+++ src/main/java/com/bank/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.bank.utils;
+
+public class Constants {
+    public static final int THREE = 3;
+    public static final int FIVE = 5;
+
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
+}
